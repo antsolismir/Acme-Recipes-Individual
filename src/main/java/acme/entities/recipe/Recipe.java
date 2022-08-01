dd021c67cf589e19b23d6d03930ed363a68d38e0
Pabgiralv <giraldillo99@gmail.com>
2022-08-01 17:22:32 +0200

    Task 024: Done

---
+++ src/main/java/acme/entities/recipe/Recipe.java
@@ -0,0 +1,52 @@
+package acme.entities.recipe;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Recipe extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	
+	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	@Column(unique=true)
+	@NotBlank
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			heading;
+
+	@NotBlank
+	@Length(max = 256)
+	protected String			description;
+	
+	@NotBlank
+	@Length(max = 256)
+	protected String			preparationNotes;
+
+	@URL
+	protected String			info;
+	
+	// Derived attributes -----------------------------------------------------
+	
+	// Relationships ----------------------------------------------------------
+
+
+}
+
