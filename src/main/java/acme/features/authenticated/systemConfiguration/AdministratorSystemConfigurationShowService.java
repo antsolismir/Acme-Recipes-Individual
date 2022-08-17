834ca331946eaa0573208b50396a5069117db523
Antonio Manuel <antomanu2000@hotmail.es>
2022-08-17 13:36:49 +0200

    Default currency fix

--- src/main/java/acme/features/authenticated/systemConfiguration/AdministratorSystemConfigurationShowService.java
+++ src/main/java/acme/features/authenticated/systemConfiguration/AdministratorSystemConfigurationShowService.java
@@ -10,7 +10,7 @@
  * they accept any liabilities with respect to them.
  */
 
-package acme.features.administrator.configuration;
+package acme.features.authenticated.systemConfiguration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
