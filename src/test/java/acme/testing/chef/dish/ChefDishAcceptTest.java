24b5e5da7c3996108e709cd9c07cbd7b03a3eda3
Antonio Manuel <antomanu2000@hotmail.es>
2022-09-02 21:42:57 +0200

    Remaining tests

---
+++ src/test/java/acme/testing/chef/dish/ChefDishAcceptTest.java
@@ -0,0 +1,66 @@
+package acme.testing.chef.dish;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class ChefDishAcceptTest extends TestHarness {
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/chef/dish/chef-dish-accept-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void ChefDishAcceptPositiveTest(final int recordIndex, final String code, final String request, final String budget, final String initialPeriodDate, final String finalPeriodDate, final String link) {
+		
+		super.signIn("epicure5", "epicure5");
+		
+		super.clickOnMenu("Epicure", "List my dishes");
+		super.checkListingExists();
+
+		super.clickOnButton("Create");
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("request", request);
+		super.fillInputBoxIn("budget", budget);
+		super.fillInputBoxIn("initialPeriodDate", initialPeriodDate);
+		super.fillInputBoxIn("finalPeriodDate", finalPeriodDate);
+		super.fillInputBoxIn("link", link);
+		
+		super.clickOnSubmit("Create");
+		
+		super.checkListingExists();
+		super.sortListing(1, "desc");
+		super.clickOnListingRecord(recordIndex);
+		
+		super.clickOnSubmit("Publish");
+		
+		super.signOut();
+		
+		super.signIn("chef5", "chef5");
+		
+		super.clickOnMenu("Chef", "List my dishes");
+		super.checkListingExists();
+		
+		super.checkListingExists();
+		super.sortListing(1, "desc");
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkFormExists();
+		super.checkInputBoxHasValue("status", "PROPOSED");
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("request", request);
+		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("initialPeriodDate", initialPeriodDate);
+		super.checkInputBoxHasValue("finalPeriodDate", finalPeriodDate);
+		super.checkInputBoxHasValue("link", link);
+		
+		super.clickOnSubmit("Accept");
+		
+		super.checkListingExists();
+		super.sortListing(1, "desc");
+		super.checkColumnHasValue(recordIndex, 0, "ACCEPTED");
+		
+		super.signOut();
+	}
+
+}
--- src/test/java/acme/testing/chef/dish/ChefDishListAndShowTest.java
+++ src/test/java/acme/testing/chef/dish/ChefDishListAndShowTest.java
@@ -1,4 +1,4 @@
-package acme.testing.epicure.dish;
+package acme.testing.chef.dish;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -6,28 +6,28 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.TestHarness;
 
-public class EpicureDishListAndShowTest extends TestHarness{
+public class ChefDishListAndShowTest extends TestHarness {
+	
 	@ParameterizedTest
-    @CsvFileSource(resources = "/epicure/dish/epicure-dish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+    @CsvFileSource(resources = "/chef/dish/chef-dish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
     @Order(10)
-    public void positiveEpicurePositiveTest(final int recordIndex, final String status, final String code, final String request, final String budget, final String creationDate, final String initialPeriodDate, final String finalPeriodDate, final String link, final String epicure, final String chef) {
+    public void positiveChefDishListAndShowPositiveTest(final int recordIndex, final String status, final String code, final String request, final String budget, final String creationDate, final String initialPeriodDate, final String finalPeriodDate, final String link) {
 
-        super.signIn("epicure1", "epicure1");
+        super.signIn("chef1", "chef1");
 
-        super.clickOnMenu("Epicure", "List my dishes");
+        super.clickOnMenu("Chef", "List my dishes");
         super.checkListingExists();
         super.sortListing(1, "asc");
 
         super.checkColumnHasValue(recordIndex, 0, status);
         super.checkColumnHasValue(recordIndex, 1, code);
+        super.checkColumnHasValue(recordIndex, 2, creationDate);
 
         super.clickOnListingRecord(recordIndex);
         super.checkFormExists();
         super.checkInputBoxHasValue("status", status);
         super.checkInputBoxHasValue("code", code);
         super.checkInputBoxHasValue("request", request);
-        super.checkInputBoxHasValue("budget", budget);
-//        super.checkInputBoxHasValue("creationDate", creationDate);
         super.checkInputBoxHasValue("initialPeriodDate", initialPeriodDate);
         super.checkInputBoxHasValue("finalPeriodDate", finalPeriodDate);
         super.checkInputBoxHasValue("link", link);
@@ -36,3 +36,4 @@ public class EpicureDishListAndShowTest extends TestHarness{
     }
 
 }
+
